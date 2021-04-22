936a153513388f05f562868af1833f7c295b0474
Jeffrey King <jek582@mail.harvard.edu>
2021-04-22 17:44:13 -0400

    add resources and update styling

--- templates/coordinategrid/components/LearningResources.tsx
+++ templates/coordinategrid/components/LearningResources.tsx
@@ -15,17 +15,11 @@ const LearningResources = ({ data }) => {
       <Text mt={4}>
         Some learning resources that might help you solve the project
       </Text>
-      <Grid templateColumns="repeat(2, 1fr)" gap={6} mt={4}>
+      <Grid templateColumns="1fr 1fr" gridAutoRows="max-content" gap={6} mt={4}>
         {data.resources.map((resource) => {
           return (
-            <GridItem key={resource.title}>
-              <LinkBox
-                as="article"
-                maxW="sm"
-                p="5"
-                borderWidth="1px"
-                rounded="md"
-              >
+            <GridItem key={resource.title} borderWidth="1px" rounded="md">
+              <LinkBox as="article" maxW="sm" p="5">
                 <Heading size="md">
                   <LinkOverlay href={resource.url} isExternal>
                     {resource.title}
