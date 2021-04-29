f245ecbe30ae848bd86bc4dc6f11bde005769284
Jeffrey King <jek582@mail.harvard.edu>
2021-04-28 22:20:00 -0400

    sign up for waitlist

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import {
   Text,
   Divider,
   GridItem,
+  Stack,
 } from "@chakra-ui/react";
 import Image from "next/image";
 import Link from "next/link";
@@ -30,18 +31,48 @@ export default function Home() {
           <Box mt={4}>
             <Image src="/demo_2.gif" alt="demo gif" width={500} height={500} />
           </Box>
-          <Link href="/project/1">
-            <Button colorScheme="teal" width={100} mt={4}>
-              Try It!
-            </Button>
-          </Link>
+          <Box mt={4} textAlign="center" display="inline-block">
+            <Link href="/project/1">
+              <Button colorScheme="teal" width={100}>
+                Try It!
+              </Button>
+            </Link>
+            <Box display="inline-block">
+              <Button
+                colorScheme="teal"
+                variant="outline"
+                ml={4}
+                onClick={() => {
+                  if (document) {
+                    document
+                      .getElementById("waitlist")
+                      .scrollIntoView({ behavior: "smooth" });
+                  }
+                }}
+              >
+                Join Wait List
+              </Button>
+            </Box>
+          </Box>
         </Box>
         <Divider mt={8} />
-        <Box mt={10}>
+        <Box mt={10} mb={50}>
           <Heading mb={10} size="lg">
             What makes a problem authentic?
           </Heading>
           <AuthenticTable />
+        </Box>
+        <Box id="waitlist" width={[300, 600]} margin="auto">
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLScO_0FpyvMGuCOAlhgJngKVi78fICSTsGYAvn9LyVcWudf2fQ/viewform?embedded=true"
+            width="100%"
+            height={600}
+            frameBorder="0"
+            marginHeight={0}
+            marginWidth={0}
+          >
+            Loading…
+          </iframe>
         </Box>
       </Box>
     </Box>
