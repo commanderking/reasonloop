faaf3ecb9eb9d4eee5f416ad1c4fd2a4f2c62f01
Jeffrey King <jek582@mail.harvard.edu>
2021-05-06 22:00:36 -0400

    define types for store

--- store/auth/useAuth.ts
+++ store/auth/useAuth.ts
@@ -2,6 +2,8 @@ import { useAuthStore } from "store/auth/useAuthStore";
 import { useEffect } from "react";
 import firebase from "store/auth/firebase";
 
+import shallow from "zustand/shallow";
+
 export const useAuth = () => {
   const {
     user,
@@ -9,13 +11,16 @@ export const useAuth = () => {
     hasLoadedUser,
     setHasLoadedUser,
     signOut,
-  } = useAuthStore((state) => ({
-    user: state.user,
-    setUser: state.setUser,
-    hasLoadedUser: state.hasLoadedUser,
-    setHasLoadedUser: state.setHasLoadedUser,
-    signOut: state.signOut,
-  }));
+  } = useAuthStore(
+    (state) => ({
+      user: state.user,
+      setUser: state.setUser,
+      hasLoadedUser: state.hasLoadedUser,
+      setHasLoadedUser: state.setHasLoadedUser,
+      signOut: state.signOut,
+    }),
+    shallow
+  );
 
   useEffect(() => {
     // Only run onAuthStateChange if we haven' tried to load the user before
@@ -23,7 +28,7 @@ export const useAuth = () => {
     if (!user && !hasLoadedUser) {
       unsubscribe = firebase.auth().onAuthStateChanged((user) => {
         if (!user) {
-          setUser(false);
+          setUser(null);
         }
 
         setUser(user);
