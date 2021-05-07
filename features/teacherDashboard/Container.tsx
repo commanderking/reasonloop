2d58839eff54fc42c99eb694f737d92eb17b62fe
Jeffrey King <jek582@mail.harvard.edu>
2021-05-06 21:35:30 -0400

    proper checks for what to load

--- features/teacherDashboard/Container.tsx
+++ features/teacherDashboard/Container.tsx
@@ -16,42 +16,38 @@ const TeacherDashboardContainer = () => {
     }
   });
 
-  if (user && hasLoadedUser) {
-    return (
-      <Box>
-        <Heading>My Custom Projects</Heading>
-        <Button
-          onClick={async () => {
-            await signOut();
-            router.push("/teacher/signin");
-          }}
-        >
-          Sign Out
-        </Button>
-        <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]} gap={4}>
-          {projects.map((project, index) => {
-            return (
-              <Box
-                padding={5}
-                border="1px solid lightgray"
-                borderRadius={5}
-                key={`${project.id}-project-card-${index}`}
-              >
-                <Heading fontSize="xl">{project.name}</Heading>
-                <Text mt={4}>{project.overview}</Text>
-              </Box>
-            );
-          })}
-        </Grid>
-      </Box>
-    );
-  }
-
-  if (!hasLoadedUser) {
+  if (!hasLoadedUser || !user) {
     return <div>Loading...</div>;
   }
 
-  return null;
+  return (
+    <Box>
+      <Heading>My Custom Projects</Heading>
+      <Button
+        onClick={async () => {
+          await signOut();
+          router.push("/teacher/signin");
+        }}
+      >
+        Sign Out
+      </Button>
+      <Grid templateColumns={["repeat(1, 1fr)", "repeat(2, 1fr)"]} gap={4}>
+        {projects.map((project, index) => {
+          return (
+            <Box
+              padding={5}
+              border="1px solid lightgray"
+              borderRadius={5}
+              key={`${project.id}-project-card-${index}`}
+            >
+              <Heading fontSize="xl">{project.name}</Heading>
+              <Text mt={4}>{project.overview}</Text>
+            </Box>
+          );
+        })}
+      </Grid>
+    </Box>
+  );
 };
 
 export default TeacherDashboardContainer;
