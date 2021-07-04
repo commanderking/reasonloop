4471266ab1437f3651ead20e4cad14b582dbee5b
Jeffrey King <jek582@mail.harvard.edu>
2021-07-03 20:14:00 -0400

    show table with schools and prompt

--- templates/scatterplot/Container.tsx
+++ templates/scatterplot/Container.tsx
@@ -3,7 +3,6 @@ import { Box, Heading } from "@chakra-ui/react";
 import _ from "lodash";
 
 import data from "data/high_school_2020.json";
-import AllItemsTable from "templates/scatterplot/components/AllItemsTable";
 import SchoolsSelection from "templates/scatterplot/phases/SchoolsSelection";
 import SchoolsSelectionReflection from "templates/scatterplot/phases/SchoolsSelectionReflection";
 import { phases } from "templates/scatterplot/constants/phases";
@@ -14,18 +13,25 @@ const ScatterPlotContainer = () => {
   );
 
   const schools = useMemo(() => _.sortBy(data, "name"), [data]);
+  const [addedSchools, setAddedSchools] = useState([]);
 
+  console.log("currentPhase", currentPhase);
   return (
     <Box>
       <Heading mb={8}>
         Project - Trends in Philadelphia School Districts
       </Heading>
       {currentPhase === phases.SCHOOLS_SELECTION_INITIAL.value && (
-        <SchoolsSelection schools={schools} setCurrentPhase={setCurrentPhase} />
+        <SchoolsSelection
+          schools={schools}
+          addedSchools={addedSchools}
+          setAddedSchools={setAddedSchools}
+          setCurrentPhase={setCurrentPhase}
+        />
       )}
 
       {currentPhase === phases.SCHOOLS_SELECTION_INITIAL_REFLECTION.value && (
-        <SchoolsSelectionReflection />
+        <SchoolsSelectionReflection schools={addedSchools} />
       )}
 
       {/* <AllItemsTable data={schools} /> */}
