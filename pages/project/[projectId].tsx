6c5ba32e99e6c1b51593445cdf33ad3646ef98e3
Jeffrey King <jek582@mail.harvard.edu>
2021-08-16 18:16:20 -0400

    incorporate apollo client

--- pages/project/[projectId].tsx
+++ pages/project/[projectId].tsx
@@ -1,7 +1,20 @@
 import CoordinateGridContainer from "templates/coordinategrid/Container";
 import data from "data/celltower/camden.json";
+import { useQuery, gql } from "@apollo/client";
+
+const ACTIVITIES = gql`
+  query Activities {
+    activities {
+      id
+    }
+  }
+`;
 
 const ProjectPage = () => {
+  const { loading, error, data: test } = useQuery(ACTIVITIES);
+
+  console.log("test", test);
+
   if (!data) {
     return <div>Loading Project</div>;
   }
